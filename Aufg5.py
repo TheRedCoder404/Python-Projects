e50479f8988098459fec0dd604c7f1820d9fc073
TheRedCoder404 <jm.jasper05@gmx.de>
2024-07-04 14:01:49 +0200

    Teil 2 vom grossen Struktur rework

--- Aufg5.py
+++
@@ -1,8 +0,0 @@
-umsatz = int(input("Bitte geben Sie einen Umsatz ein: "))
-
-if umsatz > 10000:
-    print(f"Der eingegebene Umsatz von {umsatz} entspricht Kategorie A")
-elif umsatz >= 1000:
-    print(f"Der eingegebene Umsatz von {umsatz} entspricht Kategorie B")
-else:
-    print(f"Der eingegebene Umsatz von {umsatz} entspricht Kategorie C")
--- aufg13.py
+++
@@ -1,9 +0,0 @@
-dualZ = input("Geben Sie bitte eine Dualzahl an: ")
-counter = len(dualZ) - 1
-dezZ = 0
-
-for i in dualZ:
-    dezZ += int(i) * (2 ** counter)
-    counter -= 1
-
-print(dezZ)
