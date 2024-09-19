ea9b0b0d1750ada93c4c23a5b5e630ab1c317d03
TheRedCoder404 <jm.jasper05@gmx.de>
2024-09-19 20:34:05 +0200

    2 funktionen zu den nuetzlichen funktionen hinzugefügt

--- TestScripts/TestCommands2.py
+++ TestScripts/TestCommands2.py
@@ -5,14 +5,4 @@ import ItemCreation
 sys.path.append('../Git/nuetzliche_functions')
 import nuetzliche_functions as nf
 
-items = []
-
-with open("RezepteRechner/test.json", "r") as outfile:
-    saveList = json.load(outfile)
-outfile.close
-
-for i in saveList:
-    items.append(ItemCreation.Item(diction=i))
-
-for i in items:
-    print(i.name)
+print(nf.binomial(4, 0, 0.6))
